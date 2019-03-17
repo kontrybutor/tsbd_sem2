660bde879920611e92a6d3d9c1244de581f59623
Rafal Massarnowski <reformatorsystemu@gmail.com>
2019-03-17 16:38:02 +0100

    JPwAD ex1 almost done

--- JPwAD/ex1.py
+++ JPwAD/ex1.py
@@ -1,6 +1,24 @@
 import numpy as np
+import pandas as pd
 
-filename = 'ecoli.data'
-data = np.genfromtxt(filename, delimiter=',', names=True, dtype=None, encoding='utf-8')
+filename = 'fertility_diagnosis.data'
 
-print (data)
+names = ['Season', 'Age', 'Childish diseases', 'Accident', 'Surgical intervention',
+         'High fevers', 'Alcohol consumption', 'Smoking habit', 'Sitting hours', 'Diagnosis']
+fertility_df = pd.read_csv(filename, sep=",", header=None, names=names)
+
+
+def print_statistics(attribute):
+    attr = fertility_df[attribute]
+    print("Median is:", attr.mean())
+    print("Max value is:", attr.max())
+    print("Min value is", attr.min())
+
+
+for name in names[:-1]:
+    print("Statistics for:", name)
+    print_statistics(name)
+    print()
+
+
+
