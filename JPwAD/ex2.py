4679ce46c376593f4d5b707d2d559618ae968d3c
Rafał <r.massarnowski@gmail.com>
2019-03-24 21:10:57 +0100

    ex2 seems to be done

---
+++ JPwAD/ex2.py
@@ -0,0 +1,72 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import shapiro, ttest_1samp, normaltest, mannwhitneyu
+
+
+def load_quakes():
+    filename = 'quakes.csv'
+    quakes_df = pd.read_csv(filename, sep=",", )
+    quakes_df = quakes_df.drop('index', 1)
+    return quakes_df
+    # depth = quakes_df['depth']
+
+
+def load_births():
+    filename = 'births.csv'
+    births_df = pd.read_csv(filename, sep=",")
+    births_df = births_df.drop('index', 1)
+    return births_df
+
+
+def plot_histogram(data):
+    plt.figure(figsize=(12, 10))
+    sns.set_style('darkgrid')
+    ax = sns.distplot(data, kde=True,)
+    ax.set(ylabel='count')
+    plt.show()
+
+
+def check_normality(data):
+    stat, p = normaltest(data)  # test for normality
+    print('statistics =', stat, 'p =', p)
+    alpha = 0.05
+    if p < alpha:
+        print('Samples are normally distributed, the null hypothesis can be rejected')
+    else:
+        print('Samples are not normally distributed, the null hypothesis cannot be rejected')
+
+
+def test_hypothesis(data, hypothesis):
+    stat, p = ttest_1samp(a=data, popmean=hypothesis)
+    alpha = 0.05
+    if p < alpha:
+        print('Reject null hypothesis, mean is less than', hypothesis)
+    else:
+        print('Accept null hypothesis')
+
+
+def main():
+    depths = load_quakes()['depth']
+
+    print("Normality test for depths...")
+    check_normality(depths)
+    plot_histogram(depths)
+    print("Mean is: ", depths.mean())
+    print("Test hypothesis for depths...")
+    test_hypothesis(depths, 10000.0)
+
+    print()
+
+    births = load_births()['births']
+    print("Normality test for births...")
+    check_normality(births)
+    plot_histogram(births)
+    print("Mean is: ", births.mean())
+    print("Test hypothesis for births...")
+    test_hypothesis(births, 300.0)
+
+
+if __name__ == "__main__":
+    main()
