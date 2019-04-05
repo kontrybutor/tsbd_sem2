99876b7983ea5e4228cd119d34b9c9010bc3b363
Rafal Massarnowski <reformatorsystemu@gmail.com>
2019-04-05 19:00:20 +0200

    session extracted

--- EDI/parser.py
+++ EDI/parser.py
@@ -1,10 +1,10 @@
 import apache_log_parser
 from pprint import pprint
-from collections import Counter
-
+from collections import Counter, defaultdict
+from datetime import datetime, MINYEAR, timedelta
 INPUT_FILE_NAME = "log.txt"
 OUTPUT_FILE_NAME = "parsed_log.txt"
-NUMBER_OF_LINES = 50
+NUMBER_OF_LINES = 50000
 
 
 def preprocess_log():
@@ -16,15 +16,18 @@ def preprocess_log():
 
 
 class Parser(object):
-    filtered_lines = []
-    list_of_extracted_lines = []
+
+    def __init__(self):
+        self.filtered_lines = []
+        self.list_of_extracted_lines = []
+        self.distinct_user_list = []
 
     def filter_log(self):
         """TBD..."""
         media_exts = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.gif', '.GIF', '.bmp', '.BMP', '.png', '.PNG', '.mpg', '.MPG']
         with open(OUTPUT_FILE_NAME) as processed_file:
             lines = [next(processed_file) for _ in range(NUMBER_OF_LINES)]
-            print("Before filtering are", len(lines), "lines.")
+            print("Przed filtracją było", len(lines), "linii.")
             for line in lines:
                 if "GET" in line and "200" in line:
                     if any(word in line for word in media_exts):
@@ -32,11 +35,10 @@ class Parser(object):
                     else:
                         self.filtered_lines.append(line)
 
-        print("After filtering there are", len(self.filtered_lines), "lines.")
+        print("Po filtracji zostało", len(self.filtered_lines), "linii.")
 
     def extract_values_into_dict(self):
         """TBD..."""
-
         keys = ['remote_host', 'request_url', 'time_received_datetimeobj', 'request_method', 'request_first_line', ]
         line_parser = apache_log_parser.make_parser("%h %t \"%r\" %s %b")
         for line in self.filtered_lines:
@@ -57,24 +59,50 @@ class Parser(object):
 
     def extract_distinct_users(self):
 
-        pprint(self.list_of_extracted_lines)
-        distinct_user_list = []
         for elem in self.list_of_extracted_lines:
-            if elem['remote_host'] not in distinct_user_list:
-                distinct_user_list.append(elem['remote_host'])
-        pprint(distinct_user_list)
+            if elem['remote_host'] not in self.distinct_user_list:
+                self.distinct_user_list.append(elem['remote_host'])
+        print("Liczba unikalnych użytkowników wynosi:", len(self.distinct_user_list))
+
+    def extract_sessions(self):
+        sessions = {}
+        user_session = {}
+        offset = timedelta(minutes=30)
+        for elem in self.list_of_extracted_lines:
+            sessions.setdefault(elem['remote_host'], [])
+            sessions[elem['remote_host']].append(elem['time_received_datetimeobj'])
+        for user, date_list in sessions.items():
+            first_timestamp = datetime(year=MINYEAR, month=1, day=1)
+            sessions_count = 0
+            cnt = 0
+            for date in date_list:
+                if date - first_timestamp > offset:
+                    if cnt > 1:
+                        sessions_count += 1
+                    first_timestamp = date
+                    cnt = 1
+                else:
+                    cnt += 1
+            if cnt > 1:
+                sessions_count += 1
+            user_session[user] = sessions_count
+
+        total = 0
+        for item in user_session.values():
+            total += item
+        print("Liczba sesji wynosi:", total)
 
 
 def main():
-    # preprocess_log()
 
     parser = Parser()
 
     parser.filter_log()
     parser.extract_values_into_dict()
-    parser.count_occurrences('remote_host')
-    parser.count_occurrences('request_url')
+    # parser.count_occurrences('remote_host')
+    # parser.count_occurrences('request_url')
     parser.extract_distinct_users()
+    parser.extract_sessions()
 
 
 if __name__ == "__main__":
