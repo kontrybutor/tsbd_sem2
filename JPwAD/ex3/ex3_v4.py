b9c75a641aa8836405539f65d326c8d89b6867b3
Rafał <r.massarnowski@gmail.com>
2019-05-12 11:11:06 +0200

    some fixes in ex3

--- JPwAD/ex3/ex3_v4.py
+++ JPwAD/ex3/ex3_v4.py
@@ -3,8 +3,10 @@ import warnings
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from timeit import default_timer as timer
 from sklearn import metrics
 from scipy.spatial.distance import cdist
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.decomposition import PCA
@@ -27,18 +29,23 @@ data = pd.DataFrame(spam_df).values.astype('float64')
 X = data[:, :-1]  # all rows, no label
 y = data[:, -1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 n_iters = [1, 10, 20, 30, 50, 70, 85, 100, 150, 300]
 scores = []
 losses = []
+start = timer()
 for n_iter in n_iters:
     model = MLPClassifier(hidden_layer_sizes=(20, 20, 20), max_iter=n_iter,)
     model.fit(X_train, y_train)
-    cv_scores = cross_val_score(model, X_test, y_test, cv=5)
+    cv_scores = cross_val_score(model, X, y, cv=5)
     # scores.append(model.score(X_test, y_test))
     scores.append(cv_scores.mean())
     losses.append(model.loss_)
+end = timer()
 plt.ylabel('cost')
 plt.xlabel('iterations')
 plt.title("Learning rate before PCA")
@@ -52,10 +59,11 @@ plt.plot(n_iters, scores)
 plt.show()
 # print("MLP Accuracy on input dataset", model.score(X_test, y_test))
 print("MLP Accuracy on input dataset: %0.2f (+/- %0.2f)" % (np.mean(scores), np.std(scores)))
+print("Elapsed time:", end - start)
 
 pca = PCA(n_components=2)
-pca.fit(X)
-pca_df = pca.fit_transform(X)
+pca.fit(X_scaled)
+pca_df = pca.fit_transform(X_scaled)
 
 pca_data = pd.DataFrame(pca_df)
 pca_data[2] = spam_df[[57]]
@@ -64,17 +72,19 @@ pca_data_val = pca_data.values.astype('float64')
 X_pca = pca_data_val[:, :-1]  # all rows, no label
 y_pca = pca_data_val[:, -1]
 
-X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(X_pca, y_pca, test_size=0.2, random_state=0)
+X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(X_pca, y_pca, test_size=0.3, random_state=0)
 n_iters = [1, 10, 20, 30, 50, 70, 85, 100, 150, 300]
 pca_scores = []
 losses = []
+start = timer()
 for n_iter in n_iters:
     model = MLPClassifier(hidden_layer_sizes=(20, 20, 20), max_iter=n_iter, )
     model.fit(X_train_pca, y_train_pca)
-    cv_scores = cross_val_score(model, X_test_pca, y_test_pca, cv=5)
+    cv_scores = cross_val_score(model, X_pca, y_pca, cv=5)
     # pca_scores.append(model.score(X_test_pca, y_test_pca))
     pca_scores.append(cv_scores.mean())
     losses.append(model.loss_)
+end = timer()
 plt.ylabel('cost')
 plt.xlabel('iterations')
 plt.title("Learning rate after PCA")
@@ -88,6 +98,7 @@ plt.plot(n_iters, pca_scores)
 plt.show()
 # print("MLP Accuracy after PCA:", model.score(X_test_pca, y_test_pca))
 print("MLP Accuracy after PCA: %0.2f (+/- %0.2f)" % (np.mean(pca_scores), np.std(pca_scores)))
+print("Elapsed time:", end - start)
 
 
 # print(spam_df.var().sort_values())
@@ -96,7 +107,7 @@ data_var = spam_df_with_3_attrs.values.astype('float64')
 X_var = data_var[:, :-1]  # all rows, no label
 y_var = data_var[:, -1]
 
-X_train_var, X_test_var, y_train_var, y_test_var = train_test_split(X_var, y_var, test_size=0.2, random_state=0)
+X_train_var, X_test_var, y_train_var, y_test_var = train_test_split(X_var, y_var, test_size=0.3, random_state=0)
 
 n_iters = [1, 10, 20, 30, 50, 70, 85, 100, 150, 300]
 var_scores = []
@@ -104,7 +115,7 @@ losses = []
 for n_iter in n_iters:
     model = MLPClassifier(hidden_layer_sizes=(20, 20, 20), max_iter=n_iter, )
     model.fit(X_train_var, y_train_var)
-    cv_scores = cross_val_score(model, X_test_var, y_test_var, cv=5)
+    cv_scores = cross_val_score(model, X_var, y_var, cv=5)
     # var_scores.append(model.score(X_test_var, y_test_var))
     var_scores.append(cv_scores.mean())
     losses.append(model.loss_)
@@ -133,7 +144,7 @@ chi_data = X_kbest_features.values
 X_chi = chi_data[:, :-1]  # all rows, no label
 y_chi = chi_data[:, -1]
 
-X_train_chi, X_test_chi, y_train_chi, y_test_chi = train_test_split(X_chi, y_chi, test_size=0.2, random_state=0)
+X_train_chi, X_test_chi, y_train_chi, y_test_chi = train_test_split(X_chi, y_chi, test_size=0.3, random_state=0)
 
 n_iters = [1, 10, 20, 30, 50, 70, 85, 100, 150, 300]
 chi_scores = []
@@ -141,7 +152,7 @@ losses = []
 for n_iter in n_iters:
     model = MLPClassifier(hidden_layer_sizes=(50, 50, 50), max_iter=n_iter, )
     model.fit(X_train_chi, y_train_chi)
-    cv_scores = cross_val_score(model, X_test_chi, y_test_chi, cv=5)
+    cv_scores = cross_val_score(model, X_chi, y_chi, cv=5)
     # chi_scores.append(model.score(X_test_chi, y_test_chi))
     chi_scores.append(cv_scores.mean())
     losses.append(model.loss_)
@@ -174,8 +185,8 @@ plt.show()
 distortions = []
 K = range(1, 10)
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(spam_df)
-    distortions.append(sum(np.min(cdist(spam_df, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+    kmeanModel = KMeans(n_clusters=k).fit(X)
+    distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
 
 # Plot the elbow
 plt.plot(K, distortions, 'bx-')
@@ -185,8 +196,8 @@ plt.title('The Elbow Method showing the optimal k for initial dataset')
 plt.show()
 
 input_kmeans = KMeans(n_clusters=5)
-input_kmeans.fit(spam_df)
-labels = input_kmeans.predict(spam_df)
+input_kmeans.fit(X)
+labels = input_kmeans.predict(X)
 centroids = input_kmeans.cluster_centers_
 print("Sum of squared distances of samples to their closest cluster center for initial dataset:", input_kmeans.inertia_)
 
@@ -195,8 +206,8 @@ print("Sum of squared distances of samples to their closest cluster center for i
 distortions = []
 K = range(1, 10)
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(pca_data)
-    distortions.append(sum(np.min(cdist(pca_data, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+    kmeanModel = KMeans(n_clusters=k).fit(X_pca)
+    distortions.append(sum(np.min(cdist(X_pca, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
 
 # Plot the elbow
 plt.plot(K, distortions, 'bx-')
@@ -206,8 +217,8 @@ plt.title('The Elbow Method showing the optimal k for PCA')
 plt.show()
 
 pca_kmeans = KMeans(n_clusters=5)
-pca_kmeans.fit(pca_data)
-labels = pca_kmeans.predict(pca_data)
+pca_kmeans.fit(X_pca)
+labels = pca_kmeans.predict(X_pca)
 centroids = pca_kmeans.cluster_centers_
 print("Sum of squared distances of samples to their closest cluster center for PCA :", pca_kmeans.inertia_)
 fig = plt.figure(figsize=(5, 5))
@@ -216,8 +227,8 @@ colors = map(lambda x: colmap[x+1], labels)
 plt.scatter(pca_data[0], pca_data[1], color=list(colors), alpha=0.5, edgecolor='k')
 for idx, centroid in enumerate(centroids):
     plt.scatter(*centroid, color=colmap[idx+1])
-plt.xlim(0, 2000)
-plt.ylim(0, 2000)
+# plt.xlim(0, 2000)
+# plt.ylim(0, 2000)
 plt.title('Cluster for PCA')
 plt.show()
 
@@ -226,8 +237,8 @@ plt.show()
 distortions = []
 K = range(1, 10)
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(data_var)
-    distortions.append(sum(np.min(cdist(data_var, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+    kmeanModel = KMeans(n_clusters=k).fit(X_var)
+    distortions.append(sum(np.min(cdist(X_var, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
 
 # Plot the elbow
 plt.plot(K, distortions, 'bx-')
@@ -237,8 +248,8 @@ plt.title('The Elbow Method showing the optimal k for attrs with smallest varian
 plt.show()
 
 var_kmeans = KMeans(n_clusters=2)
-var_kmeans.fit(data_var)
-labels = var_kmeans.predict(data_var)
+var_kmeans.fit(X_var)
+labels = var_kmeans.predict(X_var)
 centroids = var_kmeans.cluster_centers_
 print("Sum of squared distances of samples to their closest cluster center for attrs with smallest variance:", var_kmeans.inertia_)
 fig = plt.figure(figsize=(5, 5))
@@ -257,8 +268,8 @@ plt.show()
 distortions = []
 K = range(1, 10)
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(chi_data)
-    distortions.append(sum(np.min(cdist(chi_data, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+    kmeanModel = KMeans(n_clusters=k).fit(X_chi)
+    distortions.append(sum(np.min(cdist(X_chi, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
 
 # Plot the elbow
 plt.plot(K, distortions, 'bx-')
@@ -268,8 +279,8 @@ plt.title('The Elbow Method showing the optimal k for chi2 selection')
 plt.show()
 
 chi_kmeans = KMeans(n_clusters=5)
-chi_kmeans.fit(chi_data)
-labels = chi_kmeans.predict(chi_data)
+chi_kmeans.fit(X_chi)
+labels = chi_kmeans.predict(X_chi)
 centroids = chi_kmeans.cluster_centers_
 print("Sum of squared distances of samples to their closest cluster center for chi2 selection:", chi_kmeans.inertia_)
 fig = plt.figure(figsize=(5, 5))
